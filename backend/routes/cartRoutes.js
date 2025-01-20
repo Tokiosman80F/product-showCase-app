123911225990caec3cf8d2a103c3cb971af372f9
Tokiosman80F <tokiosman96@gmail.com>
2025-01-20 23:10:49 +0600

    Added features for login and registering user inside authController

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const cartController = require("../controller/cartController");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+const router = express.Router();
+
+router
+  .route("/")
+  .get(authMiddleware, cartController.getCart)
+  .post(authMiddleware, cartController.addToCart);
+
+router.route("/:id").delete(authMiddleware, cartController.removeFromCart);
+
+module.exports = router;
