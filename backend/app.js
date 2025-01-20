bfc7be9c83382872bc2529f7d66ac28e09488ee0
Tokiosman80F <tokiosman96@gmail.com>
2025-01-20 23:26:48 +0600

    backend functionality completed

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
+
 const productRouter = require("./routes/productRoutes");
+const authRouter = require("./routes/authRoutes");
+const cartRouter = require("./routes/cartRoutes");
 //MIDDLEWARE
 app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
 
 //ROUTES
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/cart", cartRouter);
 
 module.exports = app;
